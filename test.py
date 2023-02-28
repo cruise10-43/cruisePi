6841fd8a3f340e32b7c5afb40abc4dd7760dc77b
cruise10-43 <space.earth.10.katou@gmail.com>
2023-02-28 23:14:47 +0900

    Update:test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+import json
+y={'age': 33, 'height': 170, 'bmi': 22}
+f=open("textjson.txt", "w")
+y=json.dump(y,f)
+y
 list_a=['dog', 'cat', 'tiger']
 for s in list_a [ : ] :
 	if len(s)==5:
 		list_a.append(s)
 list_a
+
