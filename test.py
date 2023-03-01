5ddfa46f751711136e29222d6dff974a7486d377
cruise10-43 <space.earth.10.katou@gmail.com>
2023-03-01 23:50:20 +0900

    Update:test.py

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 import json
-y={'age': 33, 'height': 170, 'bmi': 22}
-f=open("textjson.txt", "w")
-y=json.dump(y,f)
+import math
+
+y = {'age': 33, 'height': 170, 'bmi': 22}
+f = open("textjson.txt", "w")
+json.dump(y, f)
 y
-list_a=['dog', 'cat', 'tiger']
-for s in list_a [ : ] :
-	if len(s)==5:
-		list_a.append(s)
+list_a = ['dog', 'cat', 'tiger']
+for s in list_a[:]:
+    if len(s) == 5:
+        list_a.append(s)
 list_a
 
+if __name__ == '__main__':
+    print(math.sqrt(3))
+    print(math.pow(2, 3))
+    print(math.e)
+    print(math.pi)
